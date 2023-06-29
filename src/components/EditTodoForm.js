c2c95e829139d6d4933d6a3763ed1e2abb9bb2c5
Vara prasad <varaprasadkanugula63@gmail.com>
2023-06-29 13:31:30 +0530

    ok
    todo list project using react

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -1,18 +1,11 @@
-import React,{useState} from 'react';
+import React from 'react';
 
-export const EditTodoForm = ({editTodo,task}) => {
-    const [value,setValue]=useState(task.task)
-
-    const handleSubmit=e=>{
-        e.preventDefault()
-        editTodo(value,task.id)
-        // setValue("")
-
-    }
+const EditTodoForm = () => {
   return (
-    <form className='TodoForm' onSubmit={handleSubmit}>
-        <input type='text' value={value} className='todo-input' placeholder='Update Task' onChange={(e)=>setValue(e.target.value)}/>
-        <button type='submit' className='todo-btn'>Update Task</button>
-    </form>
+    <div>
+      
+    </div>
   );
 }
+
+export default EditTodoForm;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,23 +1,10 @@
-import React,{useState} from 'react';
+import React from 'react';
 
-export const TodoForm = ({addTodo}) => {
-    const [value,setValue]=useState('')
-
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        if (value){
-          addTodo(value)
-          setValue('')
-        }
-        
-
-    }
-
-
+export const TodoForm = () => {
   return (
-    <form className='TodoForm' onSubmit={handleSubmit}>
-        <input type='text' value={value}className='todo-input' placeholder='Enter your task' onChange={(e)=>setValue(e.target.value)}/>
-        <button type='submit' className='todo-btn'>Add Task</button>
+    <form className="TodoForm">
+        <input type="text" className='todo-input' placeholder='Enter your task'></input>
+        <button type="submit" className='todo-btn'>Add Task</button>
     </form>
   );
 }
